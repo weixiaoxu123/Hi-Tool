019a3aa130d217e6f3f67c0bcc2ec1b00289d98b
weixu <290216080@qq.com>
2018-06-26 09:50:08 +0800

    提交文件

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Cell Disable' },
+  { id: 2, name: 'Link Failure' },
+  { id: 3, name: 'Sleeping Cell' },
+  { id: 4, name: 'PMD Crash' }
+];
