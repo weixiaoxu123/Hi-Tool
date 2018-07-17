b2e4871b27ac9137d0a9e945ad7b98d3a77f46ab
Xu Wei <xu.wei@ericsson.com>
2018-07-17 16:07:58 +0800

    7.17update

--- src/app/alarm/alarm.component.ts
+++ src/app/alarm/alarm.component.ts
@@ -1,7 +1,7 @@
 // 这里是kget
 import { Component, OnInit } from '@angular/core';
 import { UploaderService } from './uploader.service';
-
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-alarm',
@@ -10,7 +10,9 @@ import { UploaderService } from './uploader.service';
   providers: [ UploaderService ]
 })
 export class AlarmComponent implements OnInit {
-
+  
+   
+  
   message: string;
 
   constructor(private uploaderService: UploaderService) {}
@@ -25,8 +27,53 @@ export class AlarmComponent implements OnInit {
         }
       );
     }
-  }
-  ngOnInit(){
+    // var splitline='';S
+    var riLinkId = new Array();
+    var riPortRef1 = new Array();
+    var riPortRef2 = new Array();
+    
 
+    var reader:any,
+    target:EventTarget;
+    reader = new FileReader();//新建一个FileReader
+    reader.readAsText(file, "UTF-8");//读取文件 
+    reader.onload = function(evt:any){ //读取完文件之后会回来这里
+        var list = reader.result.split("\n");
+        for(var i=0;i<list.length-1;i++){
+          // 放在三个数组里面 进行操作
+          if(list[i].indexOf("riLinkId")==0){
+                alert(list[i]);
+              riLinkId.push(list[i]);
+                 alert(riLinkId);
+                }
+          if(list[i].indexOf("riPortRef1")==0){
+                alert(list[i]);
+            riPortRef1.push(list[i]);
+                 alert(riPortRef1);
+                }
+          if(list[i].indexOf("riPortRef2")==0){
+                alert(list[i]);
+           riPortRef2.push(list[i]);
+                alert(riPortRef2);
+              }
+           
+            //  for(var j=i;i<j+3;j++){
+            //   if(list[j].indexOf("riPortRef1")==0){
+            //     alert(list[j]);}
+            //    if(list[j].indexOf("riPortRef2")==0){
+            //       alert(list[j]);
+            //     }
+            //   i=j;
+           }
+         //}
+       // }
+        // list[0] = splitline + list[0];
+        // var fileString = evt.target.result; // 文件的内容 在这个地方进行处理。
+        // var str ='<p>'+fileString+'</p>';
+        // $('#test').append(str);
+      
   }
+}
+
+  ngOnInit(){}  
 }
